a98388cfcefaab5a2ce49c5531cd542b616a7813
cedric.decoster <cedric.decoster@gmail.com>
2022-07-11 20:22:03 +0200

    Projektdateien hinzufügen.

---
+++ SubNetTutorial1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Ajuna.NetApi;
+using System;
+using System.Threading.Tasks;
+
+namespace SubNetTutorial1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var uri = new Uri("ws://127.0.0.1:9944");
+            SubstrateClient client = new SubstrateClient(uri);
+
+            Console.WriteLine($"client.IsConnected: {client.IsConnected}");
+
+            await client.ConnectAsync();
+
+            Console.WriteLine($"client.IsConnected: {client.IsConnected}");
+
+            Console.WriteLine($"SpecName: {client.RuntimeVersion.SpecName}");
+            Console.WriteLine($"ImplName: {client.RuntimeVersion.ImplName}");
+        }
+    }
+}
